16ef2565b1d52de748062aa3d5d762ded2390718
IBrauningI <ibrauningi15@gmail.com>
2022-05-24 19:38:52 +0300

    Vector task

---
+++ Vector/Vector/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Vector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector arr = new Vector(20);
+            arr.RandomFill(1, 5);
+
+            try
+            {
+                arr[0] = 999;
+                Console.WriteLine(arr[21]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Pair[] pairs = arr.CalculateFreq();
+
+            for (int i = 0; i < pairs.Length; i++)
+            {
+                Console.Write(pairs[i] + "\n");
+            }
+            Console.WriteLine(); 
+
+            Console.WriteLine(pairs);
+            arr.RandomFill();
+            Console.WriteLine(arr);
+        }
+    }
+}
---
+++ Vector/Vector/Vector.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    class Vector
+    {
+        int[] arr;
+
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < arr.Length)
+                {
+                    return arr[index];
+                }
+                else
+                {
+                    throw new Exception("Out of range");
+                }
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+
+        public Vector(int[] arr)
+        {
+            this.arr = arr;
+        }
+
+        public Vector(int n)
+        {
+            arr = new int[n];
+        }
+
+        public Vector() { }
+
+        public void RandomFill(int a, int b)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(a, b);
+            }
+        }
+
+        public void RandomFill()
+        {
+            int index = Array.IndexOf(arr, 2);
+            Console.WriteLine(index);
+
+            Random random = new Random();
+            int x;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                while (arr[i] == 0)
+                {
+                    x = random.Next(1, arr.Length + 1);
+                    bool isExist = false;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (x == arr[j])
+                        {
+                            isExist = true;
+                            break;
+                        }
+                    }
+                    if (!isExist)
+                    {
+                        arr[i] = x;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public Pair[] CalculateFreq()
+        {
+
+            Pair[] pairs = new Pair[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                pairs[i] = new Pair(0, 0);
+
+            }
+            int countDifference = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isElement = false;
+                for (int j = 0; j < countDifference; j++)
+                {
+                    if (arr[i] == pairs[j].Number)
+                    {
+                        pairs[j].Freq++;
+                        isElement = true;
+                        break;
+                    }
+                }
+                if (!isElement)
+                {
+                    pairs[countDifference].Freq++;
+                    pairs[countDifference].Number = arr[i];
+                    countDifference++;
+                }
+            }
+
+            Pair[] result = new Pair[countDifference];
+            for (int i = 0; i < countDifference; i++)
+            {
+                result[i] = pairs[i];
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                str += arr[i] + " ";
+            }
+            return str;
+        }
+    }
+}
